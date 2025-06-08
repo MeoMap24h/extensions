5242f0974402cb45e6ea059550675261f2d4f879
MeoMap24h <meomap24h@gmail.com>
2025-06-09 01:13:07 +0700

    Update Hh3dtq3Provider.kt

--- hh3dtq3Provider/src/main/kotlin/recloudstream/Hh3dtq3Provider.kt
+++ hh3dtq3Provider/src/main/kotlin/recloudstream/Hh3dtq3Provider.kt
@@ -1,16 +1,24 @@
+package recloudstream
+
+import com.lagradost.cloudstream3.MainAPI
+import com.lagradost.cloudstream3.HomePageResponse
+import com.lagradost.cloudstream3.MainPageRequest
+import com.lagradost.cloudstream3.SearchResponse
+import com.lagradost.cloudstream3.LoadResponse
+import com.lagradost.cloudstream3.TvType
+
 class Hh3dtq3Provider : MainAPI() {
     override var mainUrl = "https://hh3dtq3.net"
     override var name = "HH3DTQ3"
-    override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
-    override var lang = "vi"
     override val hasMainPage = true
-
-    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
-        return newHomePageResponse(listOf(), false)
-    }
+    override var lang = "vi"
 
     override suspend fun search(query: String): List<SearchResponse> {
         return emptyList()
+    }
+
+    override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse? {
+        return null
     }
 
     override suspend fun load(url: String): LoadResponse? {

b106ff44a465537e112d17d98630ef9271b7e142
MeoMap24h <meomap24h@gmail.com>
2025-06-09 02:29:37 +0700

    Update build.gradle.kts

--- hh3dtq3Provider/build.gradle.kts
+++ hh3dtq3Provider/build.gradle.kts
@@ -1,24 +1,14 @@
-// use an integer for version numbers
+plugins {
+    id("com.lagradost.cloudstream3.gradle")
+}
+
 version = 3
 
 cloudstream {
-    // Các thuộc tính này đều có thể tuỳ chỉnh hoặc bỏ nếu không cần.
-
     description = "Plugin cho trang hh3dtq3.net"
     authors = listOf("MeoMap24h")
-
-    /**
-     * Status int as one of the following:
-     * 0: Down
-     * 1: Ok
-     * 2: Slow
-     * 3: Beta-only
-     **/
-    status = 1 // 1 = Ok, bạn có thể đổi nếu cần
-
-    tvTypes = listOf("Movie", "TV", "Others") // Chỉnh theo loại nội dung trang cung cấp
-
+    status = 1
+    tvTypes = listOf("Movie", "TV", "Others")
     iconUrl = "https://www.google.com/s2/favicons?domain=hh3dtq3.net&sz=%size%"
-
     isCrossPlatform = true
 }

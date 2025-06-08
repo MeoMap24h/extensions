6d1e96f958bfba1d1940d68c9b4d6e0e57cc81d2
MeoMap24h <meomap24h@gmail.com>
2025-06-09 01:06:45 +0700

    Update Hh3dtq3Provider.kt

--- hh3dtq3Provider/src/main/kotlin/recloudstream/Hh3dtq3Provider.kt
+++ hh3dtq3Provider/src/main/kotlin/recloudstream/Hh3dtq3Provider.kt
@@ -1,12 +1,4 @@
-package recloudstream
-
-import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.utils.AppUtils.tryParseJson
-import com.lagradost.cloudstream3.utils.StringUtils.encodeUri
-import com.lagradost.cloudstream3.utils.loadExtractor
-
 class Hh3dtq3Provider : MainAPI() {
-
     override var mainUrl = "https://hh3dtq3.net"
     override var name = "HH3DTQ3"
     override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
@@ -14,44 +6,14 @@ class Hh3dtq3Provider : MainAPI() {
     override val hasMainPage = true
 
     override suspend fun getMainPage(page: Int, request: MainPageRequest): HomePageResponse {
-        // TODO: Viết hàm crawl dữ liệu trang chủ từ hh3dtq3.net
-        // Ví dụ mẫu:
-        // val response = app.get("$mainUrl/path").document
-        // val homeList = listOf(
-        //     HomePageList(
-        //         "Danh sách phim mới",
-        //         listOf(/* SearchResponse */),
-        //         false
-        //     )
-        // )
-        // return newHomePageResponse(homeList, false)
         return newHomePageResponse(listOf(), false)
     }
 
     override suspend fun search(query: String): List<SearchResponse> {
-        // TODO: Viết hàm crawl dữ liệu tìm kiếm từ hh3dtq3.net
-        // Ví dụ mẫu:
-        // val response = app.get("$mainUrl/search?keyword=${query.encodeUri()}").document
-        // return response.select("selector").map { ... }
         return emptyList()
     }
 
     override suspend fun load(url: String): LoadResponse? {
-        // TODO: Viết hàm crawl dữ liệu chi tiết phim từ hh3dtq3.net
-        // Ví dụ mẫu:
-        // val response = app.get(url).document
-        // return newMovieLoadResponse(...)
         return null
-    }
-
-    override suspend fun loadLinks(
-        data: String,
-        isCasting: Boolean,
-        subtitleCallback: (SubtitleFile) -> Unit,
-        callback: (ExtractorLink) -> Unit
-    ): Boolean {
-        // Nếu trang có player dùng link ngoài, sử dụng loadExtractor
-        // loadExtractor("link", subtitleCallback, callback)
-        return true
     }
 }

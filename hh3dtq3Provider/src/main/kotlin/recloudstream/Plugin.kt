90c08b184fd0d3f9d7e4da9fb4363735eeb5d78e
MeoMap24h <meomap24h@gmail.com>
2025-06-09 01:00:33 +0700

    Update Plugin.kt

--- hh3dtq3Provider/src/main/kotlin/recloudstream/Plugin.kt
+++ hh3dtq3Provider/src/main/kotlin/recloudstream/Plugin.kt
@@ -1,7 +1,11 @@
-import recloudstream.Hh3dtq3Provider
+package recloudstream
 
-class Plugin : PluginMain() {
-    override fun load(context: PluginContext) {
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+
+@CloudstreamPlugin
+class Hh3dtq3Plugin : BasePlugin() {
+    override fun load() {
         registerMainAPI(Hh3dtq3Provider())
     }
 }

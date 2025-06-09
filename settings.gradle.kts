6e512930e2c7514c9cd4fa581bb8dce7d0f186d1
MeoMap24h <meomap24h@gmail.com>
2025-06-09 09:44:16 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,19 +7,4 @@ pluginManagement {
     }
 }
 
-rootProject.name = "CloudstreamPlugins"
-
-// Hàm tiện ích, đặt trước khi dùng!
-fun File.eachDir(block: (File) -> Unit) {
-    listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
-}
-
-// Danh sách disable, có thể để trống
-val disabled = listOf<String>()
-
-// Tự động include tất cả plugin có build.gradle.kts
-File(rootDir, ".").eachDir { dir ->
-    if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
-        include(dir.name)
-    }
-}
+rootProject.name

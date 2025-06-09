21a1e7f08fd888adabc035751a764c65ef00724d
MeoMap24h <meomap24h@gmail.com>
2025-06-09 09:45:35 +0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,4 +7,16 @@ pluginManagement {
     }
 }
 
-rootProject.name
+rootProject.name = "CloudstreamPlugins"
+
+fun File.eachDir(block: (File) -> Unit) {
+    listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
+}
+
+val disabled = listOf<String>()
+
+File(rootDir, ".").eachDir { dir ->
+    if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
+        include(dir.name)
+    }
+}

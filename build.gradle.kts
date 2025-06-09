3b1fa40c2833fea8d74bbc050b8152afd892e27b
MeoMap24h <meomap24h@gmail.com>
2025-06-09 07:48:50 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,3 @@
-pluginManagement {
-    repositories {
-        maven { url = uri("https://maven.cloudstream.cf/releases") }
-        gradlePluginPortal()
-        google()
-        mavenCentral()
-    }
-}
-
-plugins {
-    // Không cần khai báo gì ở đây
-}
-
 allprojects {
     repositories {
         maven { url = uri("https://maven.cloudstream.cf/releases") }

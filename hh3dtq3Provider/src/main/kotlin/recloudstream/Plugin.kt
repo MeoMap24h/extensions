b641b8f6e916c4c063428a8d4686801ce180ee0f
MeoMap24h <meomap24h@gmail.com>
2025-06-09 09:51:16 +0700

    Update Plugin.kt

--- hh3dtq3Provider/src/main/kotlin/recloudstream/Plugin.kt
+++ hh3dtq3Provider/src/main/kotlin/recloudstream/Plugin.kt
@@ -1,14 +1,11 @@
-plugins {
-    id("cloudstream-plugin")
-}
+package recloudstream
 
-version = 1 // Lần sau tăng số này lên
+import com.lagradost.cloudstream3.*
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 
-cloudstream {
-    description = "Plugin cho trang hh3dtq3.net"
-    authors = listOf("MeoMap24h")
-    status = 1
-    tvTypes = listOf("Movie", "TV", "Others")
-    iconUrl = "https://www.google.com/s2/favicons?domain=hh3dtq3.net&sz=%size%"
-    isCrossPlatform = true
+@CloudstreamPlugin
+class Plugin : CloudstreamPlugin() {
+    override fun load(context: Context) {
+        registerMainAPI(Hh3dtq3Provider())
+    }
 }
